f9363b4882ac4b6753eb4e8da699bc8acdc7a6ae
liu bo <18838978631@163.com>
2018-08-11 13:17:15 +0800

    'first'

---
+++ src/app.js
@@ -0,0 +1,9 @@
+import React,{Component} from 'react';
+import {Button} from 'antd-mobile'
+class App extends Component {
+    render(){
+        return <Button type='primary'>学习</Button>
+    }
+}
+
+export default App
